4e9296ca3ebcbfff52bfe3f8c79cd2815cbf5546
Isheanopa M <imay.pubdev@gmail.com>
2022-07-22 15:16:49 +0200

    added setAttributes helper

--- script.js
+++ script.js
@@ -9,19 +9,34 @@ const loader = document.getElementById('loader');
 
 let photosArray = [];
 
+
+
+// setAttributes Helper function
+function setAttributes(element, attributes) {
+   for (const key in attributes) {
+      element.setAttribute(key, attributes[key]);
+   }
+}
+
 // Create elements for links & Photos, Add to DOM
 function displayPhotos() {
    // run function for each object in photosArray
    photosArray.forEach((photo) => {
       // create <a> to unsplash
       const item = document.createElement('a');
-      item.setAttribute('href', photo.links.html)
-      item.setAttribute('target', '_blank');
+      setAttributes(item,
+         {
+            href: photo.links.html,
+            target: '_blank'
+         });
       // Create img for photo
       const img = document.createElement('img');
-      img.setAttribute('src', photo.urls.regular)
-      img.setAttribute('alt', photo.alt_description)
-      img.setAttribute('title', photo.alt_description)
+      setAttributes(item,
+         {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+         });
       // Put img inside <a>, then put both in imgCont El
       item.appendChild(img);
       imageContainer.appendChild(item);
