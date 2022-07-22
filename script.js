21bae8a58bbf62ac466aa31b6c2ac5c4ad3eec65
Isheanopa M <imay.pubdev@gmail.com>
2022-07-22 14:08:04 +0200

    returned from hiatus. removed statically defined images objects.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-window.console.log('testing')
+// Unsplash API
+const count = 25;
+const apiKey = 'HFqpXZvvLU-mQy2Nz7H9kosFeKa4rfe-qu_z_psKStI'
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// Get photos from unsplash api
+async function getPhotos() {
+   try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      console.log(data);
+
+   } catch (err) {
+      // Catch error here
+   }
+}
+
+async function displayPhotos() {
+
+}
+
+// On Loader
+
+getPhotos()
